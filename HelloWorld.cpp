51f8a125f92a3b49ec516c6ec2d7a818959e6027
SenadBajramovi <68155038+SenadBajramovi@users.noreply.github.com>
2020-07-11 16:40:47 +0200

    Add files via upload

---
+++ HelloWorld.cpp
@@ -0,0 +1,27 @@
+#include <SFM/Graphics.hpp>
+#include <time.h>
+using namespace sf;
+
+
+
+int main() 
+{
+   RenderWindow window(VideoMode(320, 480), "The Game!");
+
+
+   while (window.isOpen())
+   {
+       Event e;
+       while (window.pollEvent(e))
+       {
+          if (e.type == Event::Closed)
+              window.close();
+       }
+
+   window.clear(Color::White);
+   window.display();
+   
+   }
+
+   return 0;
+}	
